905b7a83328e0b31ae94dbe11f01690d6ddebee5
rikiel <rikkouambeng05@gmail.com>
2026-05-08 01:58:44 -0400

    chore: downgrade Kubeflow Pipelines version to 2.4.0 in deployment script

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -52,7 +52,7 @@ echo ""
 # Kubeflow Pipelines Setup
 ########################################
 echo "Installing Kubeflow Pipelines..."
-export PIPELINE_VERSION=2.15.0
+export PIPELINE_VERSION=2.4.0
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
 kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
